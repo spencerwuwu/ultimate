ae7ea72f6d2eb990455e753b76cf5f136759fbc1
Wei-Cheng Wu <spencerwu85@gmail.com>
2018-08-21 07:59:03 +0000

    Update .gitignore

--- script/testSingle.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-#Based_dir="/home/ultimate/ultimate/releaseScripts/default/UTaipan-linux"
-Based_dir="/home/ultimate/origin-ultimate/releaseScripts/default/UTaipan-linux"
-Ultimate="$Based_dir/Ultimate"
-Config="/home/ultimate/ultimate/script/Termination.xml"
-file=$1
-$Ultimate -tc $Config -i $file | egrep "RESULT|AUTO"
